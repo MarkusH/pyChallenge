0b2050ab712b5fb7d82ebd47f6077427c357aacd
Markus Döllinger <markus_doellinger@t-online.de>
2011-05-27 17:07:08 +0200

    Implemented test for command-line based user interface.

--- pychallenge.py
+++ pychallenge.py
@@ -2,7 +2,11 @@
 
 import pychallenge
 from pychallenge.models import Algorithm
+from pychallenge.ui import parser
 
+parser.parse()
+
+"""
 a = Algorithm(name='foo',description='foo_desc', algorithm_type_id=11)
 b = Algorithm(name='bar_name',description='bar_desc', algorithm_type_id=21)
 a.save()
@@ -15,3 +19,4 @@ b.description = 'bar_desc2'
 b.algorithm_type_id = 22
 a.save()
 b.save()
+"""
