e48cc8b54d520a14fc6168851a25e831f0be2889
Markus Holtermann <markus@markusholtermann.de>
2011-06-07 00:46:11 +0200

    fix some errors and add comparison operators for IN and NOT IN

--- pychallenge/utils/db.py
+++ pychallenge/utils/db.py
@@ -37,7 +37,7 @@ class KeyTable():
             return value
         raise StopIteration
 
-
+"""
 class LogicalOperator():
 
     def __init__(self, **kwargs):
@@ -62,7 +62,7 @@ class Or(LogicalOperator):
 
     def __repr__(self):
         return " (" + " OR ".join(self.expression_list) + ") "
-
+"""
 
 class Query():
 
@@ -130,6 +130,12 @@ class Query():
         """
         flds = kwargs.pop('fields', '*')
         self.select_fields += self._get_select_fields(flds, aggregate=True)
+        # unique `self.select_fields`
+        tmp = {}
+        for e in self.select_fields:
+            tmp[e] = 1
+        self.select_fields = tmp.keys()
+        
         print self.select_fields
 
 
@@ -158,8 +164,12 @@ class Query():
         pass
 
     def _get_filter_fields(self, **kwargs):
+
         def build(f, id, op):
             return '%s %s :%s' % (f, op, id)
+
+        def lbuild(f, id, op):
+            return '%s %s (:%s)' % (f, op, id)
 
         op = {
             'lt': '<',
@@ -168,34 +178,67 @@ class Query():
             'ge': '>=',
             'gt': '>',
         }
+
+        lop = {
+            'in': 'IN',
+            'nin': 'NOT IN'
+        }
         flds = []
+        # iterate over all given filter fields
         for f, v in kwargs.iteritems():
+
+            # split the filter comparison
             parts = f.split('__')
+
+            # if no comparison is specified act as __eq
             if len(parts) == 1 and parts[0] in self.modelfields:
                 n = self.key_table.add(name=f, value=v)
-                flds.append(build(f, n, op['eq']))
+                flds.append(build(parts[0], n, op['eq']))
+
+            # there is a concrete comparison
             elif len(parts) == 2 and parts[0] in self.modelfields:
+
+                # it's a simple comparision operator
                 if parts[1] in op.keys():
                     n = self.key_table.add(name=f, value=v)
-                    flds.append(build(f, n, op[parts[1]]))
+                    flds.append(build(parts[0], n, op[parts[1]]))
+
+                # comparision on lists
+                elif parts[1] in lop.keys():
+                    assert isinstance(v, (tuple, list))
+                    n = self.key_table.add(name=f, value=v)
+                    flds.append(lbuild(parts[0], n, lop[parts[1]]))
+
+                # this comparision operator is unknown
                 else:
                     raise AttributeError("Operator %s unknown" %
                         parts[1].upper)
+
+            # stupid user :-)  --  field does not exists
             else:
                 raise AttributeError("Unknown field %s" % f)
         return flds
 
 
-    def _get_select_fields(self, fields, aggregate=False, compare=False, **kwargs):
+    def _get_select_fields(self, fields, aggregate=False, **kwargs):
         """
 
         """
+        # if we accept aggregation functions MIN, MAX, AVG, COUNT ...
         if aggregate:
             flds = []
+
+            # iterate over the list of all given fields
             for f in fields:
+
+                # split the agg. function if it exists
                 parts = f.split('__')
+
+                # we have no agg. function -> directly add the field
                 if len(parts) == 1 and parts[0] in self.modelfields:
                     flds.append(f)
+
+                # build the aggregation sql string for the agg. function
                 elif len(parts) == 2 and parts[0] in self.modelfields:
                     if parts[1] in Query.AGGREGATE:
                         f_n = parts[1].upper() + '(' + parts[0] + ') AS ' + f
@@ -203,8 +246,13 @@ class Query():
                     else:
                         raise AttributeError("Aggregate function %s unknown" %
                             parts[1].upper)
+
+                # unknown field
                 else:
                     raise AttributeError("Unknown field %s" % f)
+
+        # we don't accept an aggregation function. Just check that all given
+        # fields are allowed (along to self.modelfields)
         else:
             for f in fields:
                 if f in self.modelfields:
