91fdaa1f22076971c848e676a1210fc3dbf8c41d
Markus Döllinger <markus_doellinger@t-online.de>
2011-06-09 12:24:31 +0200

    Using csv reader to import test data (without writing to database)

--- pychallenge/ui/parser2.py
+++ pychallenge/ui/parser2.py
@@ -1,20 +1,7 @@
 import sys
 import argparse
 from pychallenge.algorithms import elo
-
-def elo1on1(args):
-    outcome = {
-        0.0: "Player 1 won",
-        1.0: "Player 2 won",
-        0.5: "Draw"
-    }
-    print "Performing ELO 1on1 with:"
-    print "\tRating of player 1:", args.elo1
-    print "\tRating of player 2:", args.elo2
-    print "\tOutcome:           ", outcome[args.outcome] 
-    print "-----------------------------"
-
-    result = elo.elo1on1(args.elo1, args.elo2, args.outcome, args.factor, lambda x: (1 / (1 + (10 ** (x / 400.0)))))
+import csv
 
 #####################################################################
 #                           ELO sub commands                        #
@@ -45,8 +32,26 @@ def elo_add_result(args):
 def elo_import(args):
     if (not elo_prepare_args(args)):
         return
+
+    outcome = {
+        0.0: "Player 1 won",
+        1.0: "Player 2 won",
+        0.5: "Draw"
+    }
+
     print "Importing results to ELO", args.game
     print "\tFile:", args.file
+
+    try:
+        line = 0
+        reader = csv.reader(open(args.file, 'rb'), delimiter=',')
+        print "Importing the following data:"
+        for row in reader:
+            if line != 0:
+                print "Player1: " + row[1] + "; Player2: " + row[2] + "; " + outcome[float(row[3])]
+            line = line + 1
+    except:
+        print "Error importing", args.file, "in line", line
 
 def elo_update(args):
     if (not elo_prepare_args(args)):
@@ -68,13 +73,7 @@ def elo_value(args):
 def parse():
     parser = argparse.ArgumentParser(prog='pyChallenge')
     subparsers = parser.add_subparsers(help='sub-command help')
-    """
-    parser_elo1on1 = subparsers.add_parser('elo1on1', help='Calculare 1on1 match with ELO')
-    parser_elo1on1.add_argument('elo1', type=int, help='ELO rating of player 1')
-    parser_elo1on1.add_argument('elo2', type=int, help='ELO rating of player 2')
-    parser_elo1on1.add_argument('outcome', type=float, help="Outcome of the game: 0 = player 1 lost; 0.5 = draw; 1 = player 1 won")
-    parser_elo1on1.set_defaults(func=elo1on1)
-    """	
+    
     # ELO sub-commands
     p_elo = subparsers.add_parser('elo', help='Commands for the ELO algorithm')
     p_elo.add_argument('--game', help='Choose a game for ELO')
