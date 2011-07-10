772fbbff054e21cb50523f9aee59c5f669c94c0d
Markus Holtermann <markus@markusholtermann.de>
2011-07-10 23:54:18 +0200

    add comments for pychallenge.db

--- pychallenge/db/__init__.py
+++ pychallenge/db/__init__.py
@@ -3,5 +3,13 @@ import sqlite3
 from pychallenge.conf import settings
 
 
+"""
+The :py:module:`pychallenge.db` module provides a connection and
+connection-cursor object to access the database
+"""
+
+#: This is the connection object to acces the SQLite database
 connection = sqlite3.connect(settings.SETTINGS['DATABASE'])
+
+#: This is the current cursor object for the session
 db = connection.cursor()
