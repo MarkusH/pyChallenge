2c1f2ac2f0424f382950fa1cc585b2fc33db63df
fbausch <studium@fbausch.de>
2011-07-12 23:51:46 +0200

    Code formatting in GUI

--- pychallenge-qt.py
+++ pychallenge-qt.py
@@ -14,17 +14,20 @@ class PyChallengeGUI(QtGui.QWidget):
         self.qtext = QtGui.QTextEdit(self)
         self.qtext.setReadOnly(True)
 
-        self.qpb1 = QtGui.QPushButton("Get Rating",self)
+        self.qpb1 = QtGui.QPushButton("Get Rating", self)
         self.qpb1.setMinimumSize(self.qpb1.sizeHint())
 
-        self.qpb2 = QtGui.QPushButton("Compare",self)
+        self.qpb2 = QtGui.QPushButton("Compare", self)
         self.qpb2.setMinimumSize(self.qpb2.sizeHint())
 
-        self.qpb3 = QtGui.QPushButton("Worst",self)
+        self.qpb3 = QtGui.QPushButton("Worst", self)
         self.qpb3.setMinimumSize(self.qpb3.sizeHint())
 
-        self.qpb4 = QtGui.QPushButton("Best",self)
-        self.qpb4.setMinimumSize(self.qpb4.sizeHint()) 
+        self.qpb4 = QtGui.QPushButton("Best", self)
+        self.qpb4.setMinimumSize(self.qpb4.sizeHint())
+
+        self.qpb5 = QtGui.QPushButton("Update", self)
+        self.qpb5.setMinimumSize(self.qpb5.sizeHint())
 
         self.qedit1 = QtGui.QLineEdit("1",self)
         self.qedit2 = QtGui.QLineEdit("2",self)
@@ -56,41 +59,62 @@ class PyChallengeGUI(QtGui.QWidget):
         layout2 = QtGui.QHBoxLayout(self.frame1)
         layout2.addWidget(self.qpb1)
         layout2.addWidget(self.qpb2)
-        layout2.addWidget(self.qpb3)
         layout2.addWidget(self.qpb4)
+        layout2.addWidget(self.qpb3)
+        layout2.addWidget(self.qpb5)
 
         layout3 = QtGui.QHBoxLayout(self.frame2)
+        layout3.addWidget(self.qcb)
+        layout3.addWidget(self.qcb2)
         layout3.addWidget(qlab1)
         layout3.addWidget(self.qedit1)
         layout3.addWidget(qlab2)
         layout3.addWidget(self.qedit2)
         layout3.addWidget(qlab3)
         layout3.addWidget(self.qedit3)
-        layout3.addWidget(self.qcb)
-        layout3.addWidget(self.qcb2)
 
         layout.addWidget(self.qtext)
         layout.activate()
 
-        QtCore.QObject.connect(self.qpb1, QtCore.SIGNAL('clicked()'), self.rating)
-        QtCore.QObject.connect(self.qpb2, QtCore.SIGNAL('clicked()'), self.compare)
-        QtCore.QObject.connect(self.qpb3, QtCore.SIGNAL('clicked()'), self.worst)
-        QtCore.QObject.connect(self.qpb4, QtCore.SIGNAL('clicked()'), self.best)
+        QtCore.QObject.connect(self.qpb1, QtCore.SIGNAL('clicked()'),
+            self.rating)
+        QtCore.QObject.connect(self.qpb2, QtCore.SIGNAL('clicked()'),
+            self.compare)
+        QtCore.QObject.connect(self.qpb3, QtCore.SIGNAL('clicked()'),
+            self.worst)
+        QtCore.QObject.connect(self.qpb4, QtCore.SIGNAL('clicked()'),
+            self.best)
+        QtCore.QObject.connect(self.qpb5, QtCore.SIGNAL('clicked()'),
+            self.update)
 
         self.setGeometry(300, 300, 700, 450)
         self.setWindowTitle('pyChallenge - Graphical User Interface')
 
     def compare(self):
-        self.qtext.setText(Popen(["pychallenge.py", "-g", self.qcb.currentText(), "-a", self.qcb2.currentText(), "compare", self.qedit1.text(), self.qedit2.text()], stdout=PIPE).communicate()[0])
+        self.qtext.setText(Popen(["./pychallenge.py", "-g",
+            self.qcb.currentText(), "-a", self.qcb2.currentText(),
+            "compare", self.qedit1.text(), self.qedit2.text()], stdout=PIPE)
+            .communicate()[0])
 
     def rating(self):
-        self.qtext.setText(Popen(["pychallenge.py", "-g", self.qcb.currentText(), "-a", self.qcb2.currentText(),  "rating", self.qedit1.text()], stdout=PIPE).communicate()[0])
+        self.qtext.setText(Popen(["./pychallenge.py", "-g",
+            self.qcb.currentText(), "-a", self.qcb2.currentText(),  "rating",
+            self.qedit1.text()], stdout=PIPE).communicate()[0])
 
     def worst(self):
-        self.qtext.setText(Popen(["pychallenge.py", "-g", self.qcb.currentText(), "-a", self.qcb2.currentText(),  "worst", self.qedit3.text()], stdout=PIPE).communicate()[0])
+        self.qtext.setText(Popen(["./pychallenge.py", "-g",
+            self.qcb.currentText(), "-a", self.qcb2.currentText(),  "worst",
+            self.qedit3.text()], stdout=PIPE).communicate()[0])
 
     def best(self):
-        self.qtext.setText(Popen(["pychallenge.py", "-g", self.qcb.currentText(), "-a", self.qcb2.currentText(),  "best", self.qedit3.text()], stdout=PIPE).communicate()[0])
+        self.qtext.setText(Popen(["./pychallenge.py", "-g",
+            self.qcb.currentText(), "-a", self.qcb2.currentText(),  "best",
+            self.qedit3.text()], stdout=PIPE).communicate()[0])
+
+    def update(self):
+        self.qtext.setText(Popen(["./pychallenge.py", "-g",
+            self.qcb.currentText(), "-a", self.qcb2.currentText(), "update"],
+            stdout=PIPE).communicate()[0])
 
 
 if __name__ == "__main__":
